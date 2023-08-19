b1dade2e116207d563467aab7609359e61a89728
lolamamatha <141418267+lolamamatha@users.noreply.github.com>
2023-08-19 23:00:55 +0530

    Update serverdetail.sh

--- serverdetail.sh
+++ serverdetail.sh
@@ -1,35 +1,50 @@
-#!/bin/bash
+pipeline{
+    agent any
 
-echo "Server Details:"
-echo "----------------"
+        stages{
+             stage( 'build-system details'){
+               steps{
+                   sh '''
+                    echo '"ip adress"'
+                    hostname -I
+                      '''
+                 }
+             } 
+             stage('build-memory details'){
+                 steps{
+                     sh '''
+                     echo "MEMORY DETAILS"
+                     free
+                     '''                
+                }
+            }
+            stage('build-cpu details'){
+                steps{
+                    sh '''
+                    echo "cpu deatils"
+                    lscpu
+                    '''                  
+                }
+            }
+            stage('build-disk details'){
+                steps{
+                   sh '''
+                   echo "today date"
+                   date
+                   '''
+                        
+                }
+            }
+            stage('build-process command'){
+                 steps{
+                    sh '''
+                    echo "process command"
+                    ps -ef | head
+                    '''
+            
+                }
+            }     
 
-# Get hostname
-hostname=$(hostname)
-echo "Hostname: $hostname"
-
-# Get uptime
-uptime=$(uptime -p)
-echo "Uptime: $uptime"
-
-# Get kernel version
-kernel=$(uname -r)
-echo "Kernel Version: $kernel"
-
-# Get CPU information
-cpu_info=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | cut -d ':' -f 2-)
-echo "CPU: $cpu_info"
-
-# Get total memory
-total_memory=$(free -h | awk '/Mem:/ {print $2}')
-echo "Total Memory: $total_memory"
-
-# Get total disk space
-total_disk=$(df -h --total | awk '/total/ {print $2}')
-echo "Total Disk Space: $total_disk"
-
-# Get IP addresses
-ip_addresses=$(ip -4 addr | grep inet | awk '{print $2}')
-echo "IP Addresses:"
-echo "$ip_addresses"
-
-echo "----------------"
+         
+        }  
+}
